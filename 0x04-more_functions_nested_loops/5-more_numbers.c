1f73a2368aba35e3b5738491b684abf5d43f5a23
MichelleOffiong <offiongmichelle283@gmail.com>
2022-09-16 18:50:55 -0700

    task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,7 +14,7 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if (j >= 10)
+			if (j >- 10)
 			{
 				_putchar((j / 10) + 48);
 			}
@@ -23,6 +23,6 @@ void more_numbers(void)
 
 		_putchar('\n');
 
-		i++
+		i++;
 	}
 }
