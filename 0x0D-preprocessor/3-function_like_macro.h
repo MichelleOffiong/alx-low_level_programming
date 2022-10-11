c3ac8f6471962b8d49227661cbd3e884a66dc28f
MichelleOffiong <offiongmichelle283@gmail.com>
2022-10-10 19:27:39 -0700

    Preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef ABS_x
-#define ABS_x
-#define ABS(x) ((x < 0) ? (x * -1) : (x))
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
 #endif
