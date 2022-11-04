26053ee81360241f7620eba8b3cd2ddb1face8e9
MichelleOffiong <offiongmichelle283@gmail.com>
2022-11-04 14:18:18 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,49 +1,36 @@
-#include <stddef.h>
 #include "main.h"
 
 /**
- * check_valid_string - checks if a string has only 0's and 1's
- * @b: string to be checked
- *
- * Return: 1 if string is valid, 0 otherwise
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: binary number
+ * Return: the converted number or 0 if it fails
  */
 
-int check_valid_string(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
-	if (b == NULL)
+	unsigned int uint, place;
+	int n;
+
+	if (!b)
 		return (0);
-	while (*b)
+
+	for (n = 0; b[n]; n++)
+		;
+	n--;
+	for (place = 1, uint = 0; n >= 0; n--)
 	{
-		if (*b != '1' && *b != '0')
-			return (0);
-		b++
-	}
-
-	return (1);
-}
-
-/**
- * binary_to_uint - converts a binary to an unsigned int
- * @b: the binary number as a string
- *
- * Return: the converted value
- */
-
-unsigned int binary_to_uint(const char *b);
-{
-	unsigned int decimal = 0;
-	int str_len = 0, base = 1;
-
-	if (!check_valid_string(b))
+		if (b[n] == '0')
+		{
+			place *= 2;
+			continue;
+		}
+		else if (b[n] == '1')
+		{
+			uint += place;
+			place *= 2;
+			continue;
+		}
 		return (0);
-	while (b[str_len] != '\0')
-		str_len++;
-	while (str_len)
-	{
-		decimal += ((b[str_len - 1] - '0') * base);
-		base *= 2;
-		str_len--;
 	}
-
-	return (decimal);
+	return (uint);
 }
